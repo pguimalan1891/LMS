56fb386a8cebc402dfbe5862c3922bc0f8942ed9
coykie09 <rssegarino@gmail.com>
2017-02-02 17:09:37 +0800

    commit

---
+++ LMS/Content/datatable/jquery.dataTable.FilteringDelay.js
@@ -0,0 +1,35 @@
+﻿
+jQuery.fn.dataTableExt.oApi.fnFilteringDelay = function (oSettings, iDelay) {
+    var _that = this;
+
+    if (iDelay === undefined) {
+        iDelay = 250;
+    }
+
+    this.each(function (i) {
+        $.fn.dataTableExt.iApiIndex = i;
+        var
+            $this = this,
+            oTimerId = null,
+            sPreviousSearch = null,
+            anControl = $('input', _that.fnSettings().aanFeatures.f);
+
+        anControl.unbind('keyup').bind('keyup', function () {
+            var $$this = $this;
+            if (anControl.val().length < 3 && anControl.val().length > 0) {
+                return this;
+            } 
+            if (sPreviousSearch === null || sPreviousSearch != anControl.val()) {
+                window.clearTimeout(oTimerId);
+                sPreviousSearch = anControl.val();
+                oTimerId = window.setTimeout(function () {
+                    $.fn.dataTableExt.iApiIndex = i;
+                    _that.fnFilter(anControl.val());
+                }, iDelay);
+            }
+        });
+
+        return this;
+    });
+    return this;
+};
