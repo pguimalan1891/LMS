c6481b449bd35a988aaadfb54e69da44c871818b
angelo123 <angelo.g.serrano@gmail.com>
2017-02-07 17:35:17 +0800

    booking

---
+++ LMS/Controllers/Booking/BookingController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Configuration;
+using System.Net;
+using ServiceLayer;
+using ServiceLayer.Interface;
+using CommonClasses;
+using BusinessObjects;
+
+namespace LMS.Controllers
+{
+    public class BookingController : Controller
+    {
+        private IBookingSvc service;
+
+        public BookingController()
+            : this(new BookingSvc())
+        {
+            
+        }
+
+        public BookingController(IBookingSvc service)
+        {
+            this.service = service;
+        }
+
+        // GET: Booking
+        public ActionResult Booking()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RetrieveBookingRecords()
+        {
+            return Json(this.service.getBookingRecords());
+        }
+    }
+}
