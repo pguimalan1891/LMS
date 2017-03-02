daa18381fc94c19e536955ef5c82493c737b93fd
nbdegorio <nbdegorio@gmail.com>
2017-03-02 13:55:01 +0800

    -commit changes to Login Interface/Loan Application/Credit Investigation

---
+++ ServiceLayer/Interface/ILoanApplicationSvc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Interface
+{
+    public interface ILoanApplicationSvc
+    {
+        List<Dictionary<string, string>> GetBorrowers(string searchkey);
+    }
+}
